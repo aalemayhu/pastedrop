30036ae13b7995e12b0913f7c65e771063c95466
Alexander Alemayhu <alexander@alemayhu.com>
2018-10-31 20:23:12 +0100

    Add button for triggering get clipboard data
    To be implemented.

--- pastedrop/src/App.js
+++ pastedrop/src/App.js
@@ -21,6 +21,17 @@ const _styles = {
     color: '#42B74F',
     fontSize: 36,
   }),
+  roundButton: RX.Styles.createViewStyle({
+      margin: 16,
+      borderRadius: 16,
+      backgroundColor: '#7d88a9'
+  }),
+  buttonText: RX.Styles.createTextStyle({
+      fontSize: 16,
+      marginVertical: 6,
+      marginHorizontal: 12,
+      color: 'white'
+  }),
 };
 
 export class App extends RX.Component {
@@ -30,11 +41,18 @@ export class App extends RX.Component {
     RX.UserInterface.setMainView(this._renderView());
   }
 
+  _onPlusPressed() {
+    alert('todo get clipboard data');
+  }
+
   _renderView() {
     return (
       <RX.View style={ _styles.main }>
         <RX.View>
           <RX.Text style={ _styles.title }><RX.Text style={ _styles.name }>pastedrop</RX.Text></RX.Text>
+          <RX.Button style={ _styles.roundButton } onPress={ this._onPlusPressed }>
+            <RX.Text style={ _styles.buttonText }>+</RX.Text>
+          </RX.Button>
         </RX.View>
         <RootView />        
       </RX.View>
